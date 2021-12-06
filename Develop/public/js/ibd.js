1b4fde718aa7e421f7a4f2b2233b2d4d9ac4fd29
Darrik Crawford <darrikcrawford@yahoo.com>
2021-12-05 22:27:31 -0600

    update service-worker

--- Develop/public/js/ibd.js
+++ Develop/public/js/ibd.js
@@ -1,19 +1,18 @@
 //var to hold db connection
 let db;
 
-// establish connection called budget_tracker with IndexedDB and set it to version 1
+// establish connection called budget_tracker with IndexedDB
 const request = indexedDB.open('budget_tracker', 1)
 
-//check if db version changed
+//check for db version changed
 request.onupgradeneeded = function(event) {
-    //save reference to the database
+    //save reference to database
     const db = event.target.result;
 
-    //create an object store
+    //create object store
     db.createObjectStore('new_money_transaction', { autoIncrement: true });
 }
 
-//upon a successful
 request.onsuccess = function(event) {
     db = event.target.result;
 
@@ -25,3 +24,51 @@ request.onsuccess = function(event) {
 request.onerror = function(event) {
     console.log(event.target.errorCode);
 };
+
+//submit transaction without internet connection
+function saveRecord(record) {
+    //open new transaction
+    const transaction = db.transaction(['new_money_transaction'], 'readwrite');
+    //access object store
+    const moneyTransactionObjectStore = transaction.objectStore('new_money_transaction');
+    //add record to store
+    moneyTransactionObjectStore.add(record);
+}
+
+function uploadMoneyTransaction()  {
+    const transaction = db.transaction(['new_money_transaction'], 'readwrite');
+    const moneyTransactionObjectStore = transaction.objectStore('new_money_transaction');
+
+    const getAll = moneyTransactionObjectStore.getAll();
+
+    //getAll records from store / set to variable
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if(serverResponse.message){
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_money_transaction'], 'readwrite');
+                    const moneyTransactionObjectStore = transaction.objectStore('new_money_transaction');
+                    moneyTransactionObjectStore.clear();
+
+                    alert('All saved money transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+}
+
+//listen for app
+window.addEventListener('online', uploadMoneyTransaction);
